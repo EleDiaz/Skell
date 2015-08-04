2ecbb1285bd9d8eda51057e155086d42b9e34e90
Eleazar Diaz Delgado <eleazardzdo@gmail.com>
2015-08-04 20:46:08 +0100

    multiple clients initial support

--- src/Skell/Frontend/MsgPack.hs
+++ src/Skell/Frontend/MsgPack.hs
@@ -18,19 +18,18 @@ import           Network.Socket.ByteString
 
 import           Pipes
 import           Pipes.Lift
-import           Pipes.Concurrency
-import           Pipes.Network.TCP
+import qualified Pipes.Concurrent as P
 
 import           Skell.Types
 
 data RPCMsg = Request (Int, Int, String, [Object])
-            -- ^ Represent a 'request' from a client,
+            -- ^ Represent a 'request' from a client, 
             -- (0,msgID,methodName,parameters)
             | Response (Int, Int, Object, Object)
-            -- ^ Represent the response from server to client,
+            -- ^ Represent the response from server to client, 
             -- (1,msgID==msgID_client_request,errorObject,resultObject)
             | Notification (Int, String, [Object])
-            -- ^ Represent an notification, not is so clear its use.
+            -- ^ Represent an notification, not is so clear its use. 
             -- Client to Server and is like a Request but response by server
             deriving (Eq, Show)
 
@@ -41,20 +40,21 @@ msgPackFrontend = serve "127.0.0.1" 4000
 type Connection = (Socket, SockAddr, RPCMsg)
 
 serve :: HostName -> PortNumber -> PSkell -> IO ()
-serve host port model = withSocketsDo $ do
-  sock <- socket AF_INET Stream defaultProtocol
+serve host port model = withSocketsDo $ do 
+  sock <- socket AF_INET Stream defaultProtocol 
   addr <- inet_addr host
   bind sock $ SockAddrInet port addr
-  listen sock 1
-
+  listen sock 10
+  (output, input) <- P.spawn $ P.bounded 400
+  getRequest' output sock
   runEffect $ do
-    getRequest sock
-    >-> forever (wrapModel $ evalStateP def model)
+    P.fromInput input
+    >-> forever (wrapModel $ evalStateP def model) 
     >-> sendResponse
 
     where
       -- wrapModel :: Pipe a b s () -> Pipe (x, a) (x, c) s ()
-      wrapModel m = do
+      wrapModel m = do 
         (msgid, iSkell) <- await
         ((lift $ return iSkell) >~ m) >-> helper msgid
 
@@ -62,23 +62,47 @@ serve host port model = withSocketsDo $ do
       helper i = do x <- await
                     yield (i, x)
 
-getRequest :: Socket -> Producer (Connection, ISkell) IO ()
-getRequest sock = do
-    (sock', addr') <- liftIO $ accept sock
-    go sock' addr'
+
+getRequest' :: P.Output (Connection,ISkell) -> Socket -> IO ()
+getRequest' output sock = do
+  (sock', addr') <- liftIO $ accept sock
+  print "accepto"
+  P.forkIO $ go sock' addr'
+  getRequest' output sock
   where
-    go :: Socket -> SockAddr -> Producer (Connection, ISkell) IO ()
+    go :: Socket -> SockAddr -> IO ()
     go sock' addr' = do
-      bsOrError <- liftIO $ try (recv sock' 4096)
+      bsOrError <- try (recv sock' 4096)
+      print "recibo"
       case bsOrError of
-          Right bs ->
-            if not $ BS.null bs
-              then do
-                let Just (0, msgid, method, params) = unpack $ fromStrict bs :: Maybe (Int, Int, String, [Object])
-                yield ((sock', addr', Request (0, msgid, method, params)), INone)
-                go sock' addr'
-              else getRequest sock
-          Left (_::SomeException) -> getRequest sock
+        Right bs -> 
+          if not $ BS.null bs 
+            then do
+              let Just (0, msgid, method, params) = unpack $ fromStrict bs :: Maybe (Int, Int, String, [Object])
+              P.atomically $ (P.send output) ((sock', addr', Request (0, msgid, method, params)), INone)
+              print "envio"
+              go sock' addr'
+            else return ()
+        Left (_::SomeException) -> return ()
+
+
+-- getRequest :: Socket -> Producer (Connection, ISkell) IO ()
+-- getRequest sock = do
+--     (sock', addr') <- liftIO $ accept sock
+--     go sock' addr'
+--   where
+--     go :: Socket -> SockAddr -> Producer (Connection, ISkell) IO ()
+--     go sock' addr' = do
+--       bsOrError <- liftIO $ try (recv sock' 4096)
+--       case bsOrError of
+--           Right bs -> 
+--             if not $ BS.null bs 
+--               then do
+--                 let Just (0, msgid, method, params) = unpack $ fromStrict bs :: Maybe (Int, Int, String, [Object])
+--                 yield ((sock', addr', Request (0, msgid, method, params)), INone)
+--                 go sock' addr'
+--               else getRequest sock
+--           Left (_::SomeException) -> getRequest sock
 
 processMsg :: RPCMsg -> ISkell
 processMsg = undefined
